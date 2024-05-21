f23eed54d1200d7bbd4c99f90acfd81547d1eb11
jskinner <jskinner18@cnm.edu>
2024-05-21 09:39:58 -0600

    Updated with if and when statements.

--- src/main/java/edu/cnm/deepdive/Mode.java
+++ src/main/java/edu/cnm/deepdive/Mode.java
@@ -1,8 +1,13 @@
 package edu.cnm.deepdive;
 
+import java.util.regex.Pattern;
+
 public enum Mode {
 
-  CF, FC, CK, KC, KF, FK
+  CF, FC, CK, KC, KF, FK;
+
+  public static final Pattern PATTERN = Pattern.compile("CF|CK|FC|FK|KC|KF",
+      Pattern.CASE_INSENSITIVE);
 
 
 }
